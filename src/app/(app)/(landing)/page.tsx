3b2b1325e97290382b47426bbe65ff5894dc1ddc
Ibrahim Abdelrazik <ebrahimabdelrazik2002@gmail.com>
2025-01-03 23:34:37 +0200

    Update welcome message in landing page to enhance user personalization

--- src/app/(app)/(landing)/page.tsx
+++ src/app/(app)/(landing)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="flex-center h-[calc(100vh-111px)] flex-col gap-3">
       <h1 className="h1">Welcome, {user?.fullName}</h1>
 
-      <h1 className="h1">StoreIt - A simple file storage service</h1>
+      <h1 className="h1">StoreIt - A simple file storage service for you.</h1>
     </div>
   );
 }
