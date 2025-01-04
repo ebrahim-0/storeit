104ee7b8e0bdafa80a1b09ce56df4fe9645a0a59
Ibrahim Abdelrazik <ebrahimabdelrazik2002@gmail.com>
2025-01-04 02:14:33 +0200

    Add dayjs library and refactor formatDateTime function for improved date formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { appwriteConfig } from "./config";
+import dayjs from "dayjs";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -108,33 +109,7 @@ export const convertFileSize = (size: number, digits?: number) => {
 export const formatDateTime = (isoString: string | null | undefined) => {
   if (!isoString) return "—";
 
-  const date = new Date(isoString);
-
-  let hours = date.getHours();
-  const minutes = date.getMinutes();
-  const period = hours >= 12 ? "pm" : "am";
-
-  hours = hours % 12 || 12;
-
-  const time = `${hours}:${minutes.toString().padStart(2, "0")}${period}`;
-  const day = date.getDate();
-  const monthNames = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const month = monthNames[date.getMonth()];
-
-  return `${time}, ${day} ${month} ${date.getFullYear()}`;
+  return dayjs(isoString).format("h:mm A, D MMM YYYY");
 };
 
 export const getFileIcon = (
